7e943229b484a1eed831cec27e3a5d88592c16b1
kush.varade.19 <kush.varade.19@gmail.com>
2023-12-03 11:32:43 +0530

    Wishlisting of product is completed

--- server/app/controllers/user_controller.rb
+++ server/app/controllers/user_controller.rb
@@ -15,4 +15,41 @@ class UserController < ApplicationController
           render json: { status: 'error', message: 'User not found' }, status: :not_found
         end
     end
+
+    def whislist_product 
+        user_id = params[:userID]
+        product_id = params[:productID]
+
+        if user_id.blank? || !BSON::ObjectId.legal?(user_id)
+          render json: { status: 'error', message: 'Invalid userID' }, status: :unprocessable_entity
+          return
+        end
+
+        if product_id.blank? || !BSON::ObjectId.legal?(product_id)
+            render json: { status: 'error', message: 'Invalid Product ID' }, status: :unprocessable_entity
+            return
+          end
+
+        user = User.find(user_id)
+
+        if !user.present?
+            render json: { status: 'error', message: 'User not found' }, status: :not_found
+            return
+        end
+
+        new_wishlist = user.wishlisted || []
+
+        if new_wishlist.include?(product_id)
+            render json: { status: 'error', message: 'Product is already added to wishlist' }, status: :unprocessable_entity
+            return
+        end
+
+        new_wishlist << product_id
+
+        if user.set(wishlisted: new_wishlist)
+            render json: { status: 'error', message: 'Product is added to wishlist' }, status: :ok
+        else 
+            render json: { status: 'error', message: 'An unknown server side error occurred' }, status: :internal_server_error
+        end
+    end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/user/:userID", to: "user#get_user"
+  patch "/user/:userID/whislist", to: "user#whislist_product"
   get "/products", to: "product#get_products"
   get "/product/:productID", to: "product#get_product"
   get "/product/:productID/thumbnail", to: "product_image#get_product_thumbnail"
