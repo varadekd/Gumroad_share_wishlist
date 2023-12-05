340690c9e51b18dd8f4b0f7111053fdda6995a0d
kush.varade.19 <kush.varade.19@gmail.com>
2023-12-05 08:06:41 +0530

    Code format

--- app/src/components/shared/NoData.jsx
+++ app/src/components/shared/NoData.jsx
@@ -1,13 +1,7 @@
 export const NoData = (message) => {
-    return (
-        <div className="flex items-center justify-center">
-            { message ? (
-                <p>{message}</p>
-            ) : (
-                <p> Data not available. </p>
-            )
-
-            }
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center justify-center">
+      {message ? <p>{message}</p> : <p> Data not available. </p>}
+    </div>
+  );
+};
