b83bbccf1be1aea1fa09e638a4c79c7e3ae4f4c4
kush.varade.19 <kush.varade.19@gmail.com>
2023-12-05 08:11:51 +0530

    Syntax changed

--- app/src/util/api.js
+++ app/src/util/api.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 const baseURL = "http://localhost:3000";
 
-export function getDataFromApiAndCache(endpoint) {
+export const getDataFromApiAndCache = (endpoint) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
