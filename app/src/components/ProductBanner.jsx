30613d48e1caea54aedbe21354f74619f0c02bb5
kush.varade.19 <kush.varade.19@gmail.com>
2023-12-05 07:03:43 +0530

    Banner Image is added to product detail page

--- app/src/components/ProductBanner.jsx
+++ app/src/components/ProductBanner.jsx
@@ -1,24 +1,72 @@
 import { useEffect, useState } from "react";
+import { Carousel } from 'flowbite-react';
+import { makeAPICall } from "../util/api";
 
 const ProductBanner = ({ productID }) => {
-  const [isImageLoading, setImageLoading] = useState(true);
+  const [bannerData, setBannerData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  
   useEffect(() => {
-    setTimeout(() => {
-      // console.log("productID", productID);
-      setImageLoading(false);
-    }, 5000);
-  });
+    setIsLoading(true)
+
+    const getProductBanner = async() => {
+      try {
+        const apiRes = await makeAPICall(
+          "GET",
+          `/product/${productID}/banners`,
+          `get product banner for id: ${productID}`,
+        )
+        console.log(apiRes)
+        setBannerData(apiRes)
+      } catch (err) {
+        console.error("We got an error while fetching banner image")
+      setErrorMessage(err.message)      } finally {
+        setIsLoading(false)
+      }
+    };
+
+    getProductBanner()
+  }, []);
 
   return (
     <>
-      {isImageLoading ? (
+      {isLoading ? (
         <div className="bg-gray-200 w-full h-full animate-pulse"></div>
       ) : (
-        <img
-          src=""
+        <>
+          {bannerData && bannerData.success ? (
+            <>
+              { bannerData.data && bannerData.data.data && bannerData.data.data.bannerImages.length > 0 ? (
+                <>
+                 <Carousel>
+                 {bannerData.data.data.bannerImages.map((image) => (
+                      <img
+                      key={bannerData.data.data._id.$oid}
+          src={image}
           alt="{product.imageAlt}"
           className="w-full h-full object-cover rounded-md"
         />
+                    ))}
+                 </Carousel>
+                </>
+              ) : (
+                <>
+                  <h1>No data found</h1>
+                </>
+              )
+
+              }
+            </>
+          ) : (
+            <>
+            <h1>Error - {errorMessage}</h1>
+            </>
+          )
+
+          }
+        </>
+        
       )}
     </>
   );
