31ddfdc8600256c473d03e2996d58974cf22bf6e
kush.varade.19 <kush.varade.19@gmail.com>
2023-12-05 07:10:26 +0530

    Thumbnail is been added to projects page

--- app/src/components/ProductImage.jsx
+++ app/src/components/ProductImage.jsx
@@ -1,26 +1,72 @@
 import { useEffect, useState } from "react";
+import { makeAPICall } from "../util/api";
 
 const ProductImage = ({ productID }) => {
-  const [isImageLoading, setImageLoading] = useState(true);
+  const [thumbnailData, setThumbnailData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  
   useEffect(() => {
-    setTimeout(() => {
-      // console.log("productID", productID);
-      setImageLoading(false);
-    }, 5000);
-  });
+    setIsLoading(true)
+
+    const getProductBanner = async() => {
+      try {
+        const apiRes = await makeAPICall(
+          "GET",
+          `/product/${productID}/thumbnail`,
+          `get product thumbnail for id: ${productID}`,
+        )
+
+        setThumbnailData(apiRes)
+      } catch (err) {
+        console.error("We got an error while fetching banner image")
+      setErrorMessage(err.message)      } finally {
+        setIsLoading(false)
+      }
+    };
+
+    getProductBanner()
+  }, []);
 
   return (
     <>
-      {isImageLoading ? (
-        <div className="bg-gray-200 w-full h-full animate-pulse"></div>
-      ) : (
-        <img
-          src=""
-          alt="{product.imageAlt}"
-          className="w-full h-full object-cover rounded-md"
-        />
-      )}
-    </>
+    {isLoading ? (
+      <div className="bg-gray-200 w-full h-full animate-pulse"></div>
+    ) : (
+      <>
+        {thumbnailData && thumbnailData.success ? (
+          <>
+            { thumbnailData.data && thumbnailData.data.data && thumbnailData.data.data.thumbnail ? (
+              <>
+                    <img
+                    key={thumbnailData.data.data._id.$oid}
+        src={thumbnailData.data.data.thumbnail}
+        className="w-full h-full object-cover rounded-md"
+      />
+                
+              </>
+            ) : (
+              <>
+                <h1>No data found</h1>
+              </>
+            )
+
+            }
+          </>
+        ) : (
+          <>
+          <div className="h-full flex items-end">
+
+          <p>Error - {errorMessage}</p>
+          </div>
+          </>
+        )
+
+        }
+      </>
+      
+    )}
+  </>
   );
 };
 
